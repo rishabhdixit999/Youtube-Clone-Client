6742bf16d78ccd119c51fbc0a1a4aa79ac2137a1
rishabhdixit999 <88394267+rishabhdixit999@users.noreply.github.com>
2024-05-26 01:33:29 +0530

    Update ShowVideo.jsx

--- src/Components/ShowVideo/ShowVideo.jsx
+++ src/Components/ShowVideo/ShowVideo.jsx
@@ -9,7 +9,7 @@ function ShowVideo({vid}) {
      <Link to={`/videopage/${vid?._id}`}>
         <video 
         // src={`http://localhost:5500/${vid.filePath}`}
-        src={`https://youtubeclone5031.herokuapp.com/${vid.filePath}`}
+        src={`https://youtube-clone-server-ah3u.onrender.com/${vid.filePath}`}
         className="video_ShowVideo"
         />
      </Link>
